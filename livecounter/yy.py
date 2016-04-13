f5c3eedeb63d9b9258bb0cf2bd1a89af4bd6d986
Jack Ji <jiyuanyi1992@gmail.com>
2016-04-13 22:44:20 +0800

    f**k t and ent

--- livecounter/yy.py
+++ livecounter/yy.py
@@ -7,9 +7,40 @@
 
 import urllib2 as ul, urllib, re, json, time
 
-sleep_time = 0.5
+sleep_time = 0.1
+ent_url = 'http://www.yy.com/ent/index/pageLive.action'
+t_url = 'http://www.yy.com/more/page.action?biz=BBB&subBiz=idx&page=PPP&moduleId=MMM'
 
-def find_par(url):
+def find_t(url):
+    par_biz = re.compile(r'(?<=/)\w+$', re.X)
+    par_mid = re.compile(r'(?<=/idx/)\d+(?=")', re.X)
+    html = ul.urlopen( ul.Request(url) ).read()
+    return [ par_biz.search(url).group(),
+             par_mid.search(html).group() ]
+
+def fk_t(cate, url):
+    timer = time.ctime()
+    [ biz, mid ] = find_t(url)
+    uo = t_url
+    uo = uo.replace('BBB', biz).replace('MMM', mid)
+    page = 0
+    sum = 0
+    channel = 0
+    data = [ 1, 2 ]
+    while data:
+        page += 1
+        u = uo.replace('PPP', str(page))
+        html = ul.urlopen( ul.Request(u)).read()
+        time.sleep(sleep_time)
+        data = json.loads(html)
+        tt= data['data']['totalCount']
+        data = data['data']['data']
+        for item in data:
+            sum += item['users']
+            channel += 1
+    print ','.join([cate, timer, str(channel), str(sum)])
+
+def find_ent(url):
     par_pbiz = re.compile(r'(?<=/)\w+$', re.X)
     par_serv = re.compile(r'(?<=liveServ\W=\W")\d+(?=")', re.X)
     par_idid = re.compile(r'(?<=liveId\W=\W")\d+(?=")', re.X)
@@ -18,10 +49,9 @@ def find_par(url):
              par_serv.search(html).group(),
              par_idid.search(html).group() ] 
 
-def fk(cate, url):
+def fk_ent(cate, url):
     timer = time.ctime()
-    [ pbiz, serv, id ] = find_par(url)
-    act_url = 'http://www.yy.com/ent/index/pageLive.action'
+    [ pbiz, serv, id ] = find_ent(url)
     page = 0
     islast = 0
     sum = 0
@@ -31,7 +61,7 @@ def fk(cate, url):
         page += 1
         data = {'pbiz': pbiz, 'serv': serv, 'id': id, 'page': page}
         data = urllib.urlencode(data)
-        html = ul.urlopen( ul.Request(act_url, data)).read()
+        html = ul.urlopen( ul.Request(ent_url, data)).read()
         time.sleep(sleep_time)
         js_data = json.loads(html)
         islast = js_data['data']['isLastPage']
@@ -40,8 +70,8 @@ def fk(cate, url):
             channel += 1
     print ','.join([cate, timer, str(channel), str(sum)])
 
-data_dic = {}
-act_url = 'http://www.yy.com/ent/index/pageLive.action'
+data_ent = {}
+data_t = {}
 print ','.join(['栏目','抓取时间','频道数','在线人数'])
 
 # check index
@@ -50,7 +80,7 @@ html = ul.urlopen( ul.Request(index) ).read()
 start = html.find('yy.com"')
 end = html.find('其他')
 html = html[start:end+15]
-head_par = re.compile(r'(?<=href=")http:.*?(?=")', re.X)
+head_par = re.compile(r'(?<=href=")\W?http:.*?(?=")', re.X)
 cate_par = re.compile(r'(?<=span>).*?(?=</span)', re.X)
 
 head_match_group = head_par.search(html)
@@ -62,8 +92,15 @@ while head_match_group:
     head = head_match_group.group()
     cate = cate_match_group.group()
     head_match_group = head_par.search(html)
-    data_dic[cate] = head
+    if '/ent/' in head:
+        data_ent[cate] = head
+    if '/t/' in head:
+        data_t[cate] = head
 
-for i in data_dic:
-    if 'ent' in data_dic[i]:
-        fk(i, data_dic[i])
+for it in data_ent:
+    fk_ent(it, data_ent[it])
+
+print "    以下数据混有视频"
+
+for it in data_t:
+    fk_t(it, data_t[it])
